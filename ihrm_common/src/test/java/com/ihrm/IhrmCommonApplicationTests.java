241a222fd7dfbdea312c5ecd0b0566f74c89a646
min508 <405409990@qq.com>
2019-12-02 17:45:51 +0800

    项目架构1

---
+++ ihrm_common/src/test/java/com/ihrm/IhrmCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ihrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IhrmCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
